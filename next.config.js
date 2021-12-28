3acf07001ff3853ddd274d762681dfd504ba4543
SimHoZebs <simhozebs@gmail.com>
2021-12-28 00:26:02 -0500

    ???

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
     return config
